d79970e945e1d55652f3cc4a30cd01e4a470d968
TONYCRE8 <tonyingall.business@gmail.com>
2021-08-10 13:57:52 +0100

    add themes

---
+++ styles/themes.js
@@ -0,0 +1,17 @@
+const themes = {
+    dark: {
+        bgColor: '#222A31',
+        primaryColor: '#9CEED0',
+        secondaryColor: '#3D3092',
+        tertiaryColor: '#B480E1',
+        textColor: '#FFFFFF'
+    },
+    light: {
+        bgColor: '#FFFFFF',
+        primaryColor: '#49756D',
+        secondaryColor: '#3D3092',
+        tertiaryColor: '#B480E1',
+        textColor: '#222A31'
+    }
+}
+export default themes
