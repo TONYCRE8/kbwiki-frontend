15a78724706201a550270258a043386eaf4bd7b8
TONYCRE8 <tonyingall.business@gmail.com>
2021-08-22 14:50:27 +0100

    update datafetch

--- components/dataFetch.js
+++ components/dataFetch.js
@@ -2,16 +2,19 @@ import {useState, useEffect} from 'react'
 import axios from 'axios'
 
 export function DATA(query) {
+    const [singleData, setSingleData] = useState()
     const [data, setData] = useState([])
+    const [loaded, setLoaded] = useState(false)
     useEffect(() => {
         axios
-        .get(`${process.env.REACT_APP_STRAPI_API}/${query}`)
-        .then(response => {
-            setData(response.data)
-            console.log(response.data)
-        })
-        .catch(console.error)
+            .get(`${process.env.REACT_APP_STRAPI_API}/${query}`)
+            .then(response => {
+                setData(response.data)
+                setSingleData(response.data)
+                setLoaded(true)
+            })
+            .catch(console.error)
     }, [query])
 
-    return data
+    return {data, singleData, loaded}
 } 
