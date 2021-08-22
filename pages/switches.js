6373e201160a9145831f4c0377711e946052b021
TONYCRE8 <tonyingall.business@gmail.com>
2021-08-22 14:49:59 +0100

    implement skeletons

--- pages/switches.js
+++ pages/switches.js
@@ -2,9 +2,10 @@ import {DATA} from "../components/dataFetch"
 import Link from "next/link"
 import Layout from "../components/layout/layout"
 import Image from "next/image"
+import SkeletonSwitchList from "../components/skeletons/skeletonSwitchList"
 
 const Switches = () => {
-    const data = DATA("switches?_sort=name:DESC")
+    const switches = DATA("switches?_sort=name:DESC")
     return (
         <Layout>
           <div className="xl:w-2/5 w-2/3">
@@ -17,9 +18,9 @@ const Switches = () => {
               </div>
             </div>
             <div className="flex flex-row justify-evenly">
-              {data.map((s) => (
+              {switches.loaded && switches.data.map((s) => (
                 <Link href={`/switches/${s.slug}`} key={s.slug} id={s.slug}>
-                    <div className="flex flex-row justify-between w-80 h-28 border-r-8 rounded-md cursor-pointer" style={{borderColor: "var(--primary-color)"}}>
+                    <div className="flex flex-row justify-between w-80 h-28 border-r-8 rounded-md cursor-pointer mx-2" style={{borderColor: "var(--primary-color)"}}>
                       <Image className="rounded-lg" width={112} height={112} src={`${process.env.REACT_APP_STRAPI_API}${s.thumb.formats.thumbnail.url}`} />
                       <div className="flex flex-col mr-4 text-right justify-between h-full">
                         <h2 className="text-2xl">{s.name}</h2>
@@ -32,6 +33,9 @@ const Switches = () => {
                 </Link>
               ))}
             </div>
+            {!switches.loaded && (
+              <SkeletonSwitchList />
+            )}
           </div>
         </Layout>
     )
