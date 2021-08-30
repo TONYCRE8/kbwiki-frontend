0970212c6ec141468e85f4a599e16d12dcc49cfb
TONYCRE8 <tonyingall.business@gmail.com>
2021-08-30 14:08:22 +0100

    fix layout width

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -4,7 +4,9 @@ function Layout({children}) {
     return (
         <>
             <Header/>
-            <main className="flex m-0 p-0 items-center justify-center h-full flex-col">{children}</main>
+                <main className="flex m-0 p-0 items-center justify-center h-full flex-col">
+                    <div className="2xl:w-2/3 w-4/5 py-16">{children}</div>
+                </main>
             <Footer />
         </>
     )
