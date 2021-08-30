92ac30a7fd6f54e09f1efbe832abfe8c8612ee22
TONYCRE8 <tonyingall.business@gmail.com>
2021-08-30 15:54:41 +0100

    remove unused const

--- components/dataFetch.js
+++ components/dataFetch.js
@@ -2,7 +2,6 @@ import {useState, useEffect} from 'react'
 import axios from 'axios'
 
 export const DATA = (query) => {
-    const [singleData, setSingleData] = useState()
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(false)
