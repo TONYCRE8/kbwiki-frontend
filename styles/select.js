305bae48192feb3f2cec972d15468f24a16e432b
TONYCRE8 <tonyingall.business@gmail.com>
2021-09-05 20:51:58 +0100

    new select styles

--- styles/select.js
+++ styles/select.js
@@ -1,7 +1,9 @@
+import {DATA} from "../components/dataFetch"
+
 const selectTheme = {
     option: (provided, state) => ({
-      color: state.isSelected ? "white" : "var(--text-color)",
-      background: state.isSelected ? "var(--secondary-color)" : "var(--bg-accent)",
+      color: state.isSelected ? "white" : "#222A31",
+      background: state.isSelected ? `var(--secondary-color)` : "white",
       padding: "4px",
       width: "100px",
       textAlign: "center",
@@ -50,4 +52,69 @@ const selectTheme = {
       color: "var(--text-color)"
     }),
 }
-export default selectTheme
+const colorSelectTheme = {
+  option: (provided, state) => {
+    const data = DATA("keycap-colors").data
+    //This runs 12 times, making 12 seperate calls. We want 1 call for all the colors.
+    const color = data.find(el => el.color === state.label)
+    var hexColor = "var(--bg-accent)"
+    if (color) {
+      hexColor = `#${color.hex}`
+    }
+
+    return {
+      color: state.isSelected ? "white" : "#222A31",
+      background: state.isSelected ? `${hexColor}` : "white",
+      padding: "4px",
+      width: "100px",
+      textAlign: "center",
+      borderRadius: 16,
+      borderLeft: `${hexColor} solid 16px`,
+      margin: "2px",
+      paddingRight: "16px",
+      ":active": {
+          backgroundColor: "var(--secondary-color)",
+          color: "white"
+      }
+    }
+  },
+  control: (provided) => ({
+    ...provided,
+    background: "none",
+    border: "none",
+  }),
+  container: () => ({
+    display: "flex",
+    flexFlow: "row-reverse wrap"
+  }),
+  dropdownIndicator: () => ({
+    display: "none"
+  }),
+  indicatorSeparator: () => ({
+    display: "none"
+  }),
+  indicatorsContainer: () => ({
+    width: 100
+  }),
+  clearIndicator: (provided) => ({
+    ...provided,
+    color: "var(--text-color)"
+  }),
+  menu: () => ({
+    background: "none",
+    width: "80%"
+  }),
+  menuList: () => ({
+    display: "flex",
+    flexFlow: "row wrap",
+    width: "auto",
+  }),
+  valueContainer: (provided) => ({
+      ...provided,
+      display: "none"
+  }),
+  loadingMessage: () => ({
+    color: "var(--text-color)"
+  }),
+}
+export {selectTheme, colorSelectTheme}
