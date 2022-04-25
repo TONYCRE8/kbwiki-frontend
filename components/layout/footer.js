2e69852cb6b53855218f14dda125153f9b826a2d
TONYCRE8 <tonyingall.business@tonycre8.com>
2022-04-25 17:21:31 +0100

    resize main page layout

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -3,13 +3,15 @@ import ValaIcon from "../../public/vala"
 
 function Footer() {
     return (
-        <div className="my-0 mx-auto 2xl:w-2/3 xl:w-3/5 w-4/5">
-            <hr style={{borderColor: "var(--text-color)"}}></hr>
-            <div className="flex py-4 items-center md:justify-end justify-center">
-                <div style={{fill: "var(--text-color)"}}>
-                    <ValaIcon className="w-16 h-16"/>
+        <div className="flex m-0 p-0 items-center justify-center h-full flex-col">
+            <div className="2xl:w-4/5 w-5/6 py-16">
+                <hr style={{borderColor: "var(--text-color)"}}></hr>
+                <div className="flex py-4 items-center md:justify-end justify-center">
+                    <div style={{fill: "var(--text-color)"}}>
+                        <ValaIcon className="w-16 h-16"/>
+                    </div>
+                    <p>Brought to you by <a style={{color: "var(--tertiary-color"}} href="https://vala.supply">vala.supply</a></p>
                 </div>
-                <p>Brought to you by <a style={{color: "var(--tertiary-color"}} href="https://vala.supply">vala.supply</a></p>
             </div>
         </div>
     )
--- components/layout/header.js
+++ components/layout/header.js
@@ -1,8 +1,13 @@
 import Nav from "./nav"
+import Image from "next/image"
+import Logo from "../../public/logo-beta.svg"
 
 const Header = () => {
     return (
-        <div className="md:h-12">
+        <div className="flex flex-row md:h-12">
+            <div className="mt-2 lg:ml-8">
+                <Image src={Logo} height={72} width={72} />
+            </div>
             <Nav />
         </div>
     )
