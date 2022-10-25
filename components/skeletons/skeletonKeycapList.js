b39ce7e08a97539e58b8114c3540b29a5e950f4b
TONYCRE8 <tonyingall.business@tonycre8.com>
2022-10-25 15:47:53 +0100

    update skeleton layouts

--- components/skeletons/skeletonKeycapList.js
+++ components/skeletons/skeletonKeycapList.js
@@ -3,18 +3,18 @@ function SkeletonKeycapList() {
     return (
         <div className="flex flex-row flex-wrap justify-center">
             {n.map((n) => (
-                <div key={n} className="w-72 2xl:m-4 lg:m-1 m-4 rounded-lg cursor-pointer shadow-lg animate-pulse">
-                    <div className="w-full bg-gray-400 h-14 rounded-t-3xl" />
+                <div key={n} className="w-72 2xl:m-4 lg:m-1 m-4 rounded-lg cursor-pointer shadow-lg animate-pulse" style={{background: "var(--bg-accent)"}}>
+                    <div className="w-full bg-gray-400 h-14 rounded-t-lg" />
                     <div className="flex flex-col items-start justify-between w-72">
-                        <div className="w-48 h-6 mb-1 mt-2 rounded-lg bg-gray-400"></div>
-                        <div className="border-l-8 rounded-bl-lg flex flex-col justify-between items-end" style={{borderColor: "var(--primary-color)"}}>
-                            <div className="flex flex-row items-end justify-between w-64 ml-4 mt-2 pb-2">
+                        <div className="w-48 h-6 mb-1 mt-2 ml-1 rounded-lg bg-gray-400"></div>
+                        <div className="border-l-8 rounded-bl-lg flex flex-col justify-between items-end w-full" style={{borderColor: "var(--primary-color)"}}>
+                            <div className="flex flex-row items-end justify-between w-full mt-2 ml-1 pb-1">
                                 <div className="ml-2">
                                     <div className="h-2 mb-2 rounded-lg w-16 bg-gray-400" />
                                     <div className="h-2 mb-2 rounded-lg w-32 bg-gray-400" />
                                     <div className="h-2 mb-2 rounded-lg w-24 bg-gray-400" />
                                 </div>
-                                <div className="h-4 mb-2 rounded-lg w-24" style={{background: "var(--secondary-color)"}} />
+                                <div className="h-4 mb-2 rounded-lg w-24 mr-1" style={{background: "var(--secondary-color)"}} />
                             </div>
                         </div>
                     </div>
--- components/skeletons/skeletonSwitchList.js
+++ components/skeletons/skeletonSwitchList.js
@@ -3,14 +3,18 @@ function SkeletonSwitchList() {
     return (
         <div className="flex flex-row flex-wrap justify-center">
             {n.map((n) => (
-                <div key={n} className="flex flex-row justify-between w-80 h-28 m-4 border-r-8 rounded-md cursor-pointer animate-pulse shadow-lg" style={{borderColor: "var(--primary-color)"}}>
-                    <div className="rounded-lg w-28 h-28 bg-gray-400"/>
-                    <div className="flex flex-col mr-4 items-end justify-between h-full pb-2">
-                        <div className="h-8 w-40 rounded-lg bg-gray-400"></div>
-                        <div className="h-2 w-16 rounded-lg bg-gray-400"></div>
-                        <div className="h-2 -mt-6 w-24 rounded-lg bg-gray-400"></div>
-                        <div className="h-2 -mt-6 w-8 rounded-lg bg-gray-400"></div>
-                    </div>
+                <div key={n} className="animate-pulse flex flex-row justify-between w-72 h-32 2xl:m-4 lg:m-1 m-4 rounded-lg cursor-pointer shadow-lg transition transform duration-150 hover:-translate-y-1" style={{background: "var(--bg-accent)"}}>
+                    <div className="rounded-lg w-32 h-32 bg-gray-400"></div>
+                    <div className="flex flex-col text-right justify-between h-full">
+                        <div className="flex flex-col justify-center h-full text-right">
+                            <div className="h-8 w-36 mr-1 rounded-lg bg-gray-400"></div>
+                        </div>
+                        <div className="border-r-8 rounded-br-lg flex flex-col items-end pr-2 py-2" style={{borderColor: "var(--primary-color)"}}>
+                            <div className="h-2 w-16 rounded-lg bg-gray-400"></div>
+                            <div className="h-2 mt-2 w-16 rounded-lg bg-gray-400"></div>
+                            <div className="h-2 mt-2 w-16 rounded-lg bg-gray-400"></div>
+                        </div>
+                      </div>
                 </div>
             ))}
         </div>
