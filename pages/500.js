7dcd6ced21ab7222c2e811742c04c53b1157e66e
TONYCRE8 <tonyingall.business@tonycre8.com>
2022-10-25 16:30:18 +0100

    fix responsive 500 page

--- pages/500.js
+++ pages/500.js
@@ -7,18 +7,18 @@ import Kibi from '../public/kibi-404.svg'
 function Custom500() {
     return (
         <Layout>
-            <div className="flex flex-row-reverse justify-center items-center">
-                <div className='w-1/2'>
-                    <h1 className='text-9xl'>500</h1>
+            <div className="flex md:flex-row-reverse flex-col-reverse justify-center items-center">
+                <div className='md:w-1/2'>
+                    <h1 className='md:text-9xl text-6xl'>500</h1>
                     <hr></hr>
-                    <p className='mt-4 text-2xl'>
+                    <p className='mt-4 md:text-2xl text-lg'>
                         Whoops! Something went wrong! This page not exist or be broken, please report it to us on Discord!
                     </p>
                     <Link href="/">
-                        <button className="cursor-pointer mt-8 h-8 px-4 uppercase rounded-xl font-nunito-black text-white" style={{background: "var(--secondary-color)"}}>Let&apos;s get you back to safety, shall we?</button>
+                        <button className="cursor-pointer mt-8 md:h-8 h-16 px-4 uppercase rounded-xl font-nunito-black text-white" style={{background: "var(--secondary-color)"}}>Let&apos;s get you back to safety, shall we?</button>
                     </Link>
                 </div>
-                <div className='w-1/2'>
+                <div className='md:w-1/2'>
                     <Image src={Kibi} alt="Kibi Lost?" />
                 </div>
             </div>
